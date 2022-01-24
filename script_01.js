6fc20a6345c103985887c391fac5c5b8d51f0c9b
Anet <anetborin@mail.com>
2022-01-24 08:41:55 +0100

    freitag

--- script_01.js
+++ script_01.js
@@ -1,6 +1,30 @@
 "use strict";
 
 /* Die Satzbau-Maschine | Arrays */
+output(getSentence("Ich","bin","Max","Mütze",));
+function getSentence(word1,word2,word3,word4) {
+
+    const gap = " ";
+    const punct = ".";
+    const str = word1 + gap + 
+                word2 + gap +
+                word3 + gap +
+                word4 + 
+                punct; 
+
+    let arr;
+    arr = [];
+    arr = [2,4,7,11];
+    arr = [false,true,true];
+    arr = ["Ich","bin",]
+    output(arr);
+    
+
+
+
+    return str;
+
+}
 
 
 
